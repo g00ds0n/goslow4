c79e79b68947ddf7b07ed8403bbedf246cc62845
John Ibanez <jyibanez@icloud.com>
2015-03-22 04:33:06 -0700

    Config file template.
    Needs some work but this is a good base for now.

--- src/js/goslow-config.js
+++ src/js/goslow-config.js
@@ -1,22 +1,37 @@
 /**
  * GoSlow configuration variables
  *
- * IMPORTANT: If this app is running on the camera, then you should remove the
- *            "http://10.5.5.9:8080" text from "videos" and "live."
  */
 
 var goslow = {
+  choices: [
+    {
+      title: "High Speed",
+      mode: 'video',
+      fps: 120,
+      resolution: '1080',
+      fov: 'medium',
+      btn: 'primary',
+      count: 5,
+      playback: 10,
+    },
+    {
+      title: "Message",
+      mode: 'video',
+      fps: 24,
+      resolution: '1080',
+      fov: 'wide',
+      btn: 'warning',
+      count: 15,
+      playback: 5,
+    },
+  ],
 // Camera Variables
 //
 // videos:          The DCIM folder to find newly created videos
 //                   (usually http://10.5.5.9:8080/videos/DCIM/).
-// live:            The live stream of the camera
-//                   (usually http://10.5.5.9:8080/live/amba.m3u8).
-// wifi:            The wifi password of the camera.
 // test_mode:       true runs the program without connecting to the camera
   videos: "http://10.5.5.9:8080/videos/DCIM/",
-  live: "http://10.5.5.9:8080/live/amba.m3u8",
-  wifi: "goprohero",
   test_mode: false,
 
 // Other Variables
@@ -24,18 +39,6 @@ var goslow = {
 // show_clip:       true shows clip.mp4 (not included); false shows goslow.png
 // direction:       The position of the camera relative to the device.
 //                   Values can be up, right, left, or down.
-// live_timer:      How many seconds to display the live camera before
-//                   recording. 0 skips this entirely.
-// title:           The title that appears on the home page.
-// instructions:    Instructions that appear on the home page.
-// playback_text:   If the playback video is skipped, show this text.
-// playback_slowmo: True or false. Usually leave false because it takes about 90
-//                   seconds to transfer the file from the camera to an iPad.
-//                   Also an iPad can only play videos at 60 fps, so 120 fps will
-//                   throw an error.
   show_clip: false,
-  direction: 'up',
-  live_timer: 10,
-  playback_text: "<p>Thank you!<p style='font-size:0.8em'>Your video will be posted after the event.<br><br><p style='font-size:0.5em' class='text-danger'>Sorry, high speed videos are too large<br>to playback here.",
-  playback_slowmo: false
-}
+  direction: 'down',
+};
